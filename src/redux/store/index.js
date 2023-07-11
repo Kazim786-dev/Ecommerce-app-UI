f5e2f7dfb53bc3e6cf330fe1267dc06fe9384e00
Kazim786-dev <kazimasif2020@gmail.com>
2023-07-11 12:37:31 +0500

    ui

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,11 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 
 // redux-slice
 import cartReducer from '../slice/cart/cart-slice'
-import customerSlice from '../slice/auth/customer-slice'
+import customerReducer from '../slice/auth/customer-slice'
 
 export const store = configureStore({
 	reducer: {
 		cart: cartReducer,
-		customer: customerSlice,
+		customer: customerReducer,
 	},
 })
